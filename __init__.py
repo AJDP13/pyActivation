dc2f9cd2a2acb781f56ef1ee116d532110b6ba88
Arun Dutta-Plummer <87605133+AJDP13@users.noreply.github.com>
2021-07-23 16:49:25 +0100

    Version 0.0.1 | First Beta Release | No functionality yet, only gui windows (partially done)

--- __init__.py
+++ __init__.py
@@ -1,2 +1,19 @@
+"""
+VERSION -- 0.0.1 (BETA)
+PyActivation - Version 0.0.1 (Beta)
+Author --> Arun Dutta-Plummer (A.K.A AJDP13)
+Contributors --> Arun Dutta-Plummer (A.K.A AJDP13)
+Date Updated --> 23/07/2021
+"""
+
+__authors__ = {
+    "Arun Dutta-Plummer":"AJDP13",
+}
+__contributors__ = {
+    "Arun Dutta-Plummer":"AJDP13",
+}
+
+__Date_Updated__ = "23/07/2021"
+
 from activation import *
 from regKeys import *
