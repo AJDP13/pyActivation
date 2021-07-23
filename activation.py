0eea2bc272ce163353b1c2b79dc2a8032d6c11f7
Arun Dutta-Plummer <87605133+AJDP13@users.noreply.github.com>
2021-07-23 16:11:04 +0100

    Changed

--- activation.py
+++ activation.py
@@ -5,7 +5,7 @@ except ImportError:
 
 class Activation:
 
-    def __init__(self, title="Activation", sizeX = 300, sizeY=300, dispX=0, dispY=0, resizeX = True, resizeY = True, fullscreen = False, topmost = False):
+    def __init__(self, title="Activation", sizeX = 300, sizeY=300, dispX=0, dispY=0, resizeX = True, resizeY = True, fullscreen = False, topmost = False, alpha=1):
         self.__title = title
         self.__sizeX = sizeX
         self.__sizeY = sizeY
@@ -15,3 +15,14 @@ class Activation:
         self.__resizeY = resizeY
         self.__fullscreen = fullscreen
         self.__topmost = topmost
+        self.__showing = False
+        self.__alpha = alpha
+
+    def show(self):
+        self.__root = Tk()
+        self.__root.geometry("{}x{}+{}+{}".format(self.__sizeX, self.__sizeY, self.__dispX, self.__dispY))
+        self.__root.title(self.__title)
+        self.__root.resizable(self.__resizeX, self.__resizeY)
+        self.__root.attributes("-fullscreen", self.__fullscreen)
+        self.__root.attributes("-topmost", self.__topmost)
+        self.__root.attributes("-alpha", self.__alpha)
