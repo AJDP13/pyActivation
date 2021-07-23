b56031ddc52559d8250d482ba609109d49eed129
Arun Dutta-Plummer <87605133+AJDP13@users.noreply.github.com>
2021-07-23 16:19:13 +0100

    Commit

--- activation.py
+++ activation.py
@@ -18,7 +18,7 @@ class ActivationWindow:
         self.__showing = False
         self.__alpha = alpha
 
-    def show(self):
+    def show(self): #Function to create window and show it
         self.__root = Tk()
         self.__root.geometry("{}x{}+{}+{}".format(self.__sizeX, self.__sizeY, self.__dispX, self.__dispY))
         self.__root.title(self.__title)
@@ -26,3 +26,42 @@ class ActivationWindow:
         self.__root.attributes("-fullscreen", self.__fullscreen)
         self.__root.attributes("-topmost", self.__topmost)
         self.__root.attributes("-alpha", self.__alpha)
+
+##Functions for changing attributes
+    def fullscreen(self, option = True):
+        if self.__fullscreen == option:
+            if option:
+                print("ActivationWin: Window is already fullscreen")
+            else:
+                print("ActivationWin: Window is already not fullscreen")
+            return False
+        else:
+            self.__root.attributes("-fullscreen", option)
+            self.__fullscreen = option
+            return True
+    def alpha(self, option):
+        if not isinstance(option, float) or not isinstance(option, int):
+            print("Given argument for alpha attribute is not a valid float/integer")
+            return False
+        
+        else:
+            self.__root.attributes("-alpha", option)
+            self.__alpha = option
+            return True
+
+    def position(self, X=0, Y=0):
+        if isinstance(X, int) and isinstance(Y, int):
+            self.__root.geometry("{}x{}+{}+{}".format(self.__sizeX, self.__sizeY, X, Y))
+            self.__dispX = X
+            self.__dispY = Y
+            return True
+        else:
+            print("ActivationWin: Given arguments for X and Y position are not valid integers")
+            return False
+
+
+
+##Functions for controlling window
+    def destroy(self):
+        self.__showing = False
+        self.__root.destroy()
