df1952ea80ad93f58935eb5bf0044d11ad5e87b6
Arun Dutta-Plummer <87605133+AJDP13@users.noreply.github.com>
2021-07-23 10:23:59 +0100

    Commited

--- activation.py
+++ activation.py
@@ -2,3 +2,10 @@ try:
     from tkinter import Tk, Label, Button
 except ImportError:
     print("Error importing reuired library tkinter")
+
+class Activation:
+
+    def __init__(self, title="Activation", sizeX = 300, sizeY=300, dispX=0, dispY=0, resizeX = True, resizeY = True, fullscreen = False, topmost = False):
+        self.__title = title
+        self.__sizeX = sizeX
+        self.__sizeY = sizeY
